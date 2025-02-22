3a296519d95f5c18c881b0ab6c3a065285c5973f
fachridp <basix.folix@gmail.com>
2025-02-22 10:11:25 +0800

    Fix the weird layout in the active filter anime.

--- src/components/ActiveFiltersAnime.jsx
+++ src/components/ActiveFiltersAnime.jsx
@@ -53,7 +53,7 @@ export default function ActiveFiltersAnime() {
   <>
    {(mergedSelectedFilters.length > 0 || sliderFilterHasChanged) && (
     <div className="flex items-center font-semibold mb-4 text-white text-xs gap-x-2">
-     <div className="flex flex-1 group/clearAll gap-x-3">
+     <div className="flex flex-1 group/clearAll gap-x-3 flex-wrap gap-y-2">
       <svg className='w-5 fill-gray-500' aria-hidden="true" focusable="false" data-icon="tags" role='img' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 91 73">
        <path fillRule="evenodd" d="M40.3 67.78a1.825 1.825 0 0 1-2.578 0L1.036 31.092a1.71 1.71 0 0 1-.528-1.441L2.625 4.494a1.8 1.8 0 0 1 1.664-1.663L29.446.713c.062 0 .125-.008.186-.008a1.688 1.688 0 0 1 1.255.536l36.688 36.688a1.826 1.826 0 0 1 0 2.579L40.3 67.78Zm49.668-23.287a1.83 1.83 0 0 0 0-2.58L53.282 5.226a1.7 1.7 0 0 0-1.444-.528l-11.162.94 29.6 29.6a5.64 5.64 0 0 1 0 7.971L50.914 62.561l9.2 9.2a1.832 1.832 0 0 0 2.578 0l27.276-27.268ZM11.844 12.048a6.009 6.009 0 1 0 8.5 8.496 6.009 6.009 0 0 0-8.5-8.496Zm5.8 5.8a2.2 2.2 0 1 0-1.553.644 2.2 2.2 0 0 0 1.555-.641l-.002-.003Z" clipRule="evenodd" />
       </svg>
