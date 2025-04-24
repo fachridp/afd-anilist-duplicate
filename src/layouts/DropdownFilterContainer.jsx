0f48a28b5a1ac4ce0094e37f7958cae682f1031c
fachridp <basix.folix@gmail.com>
2025-04-24 08:50:31 +0800

    Fix the z-index of the anime input filters.

--- src/layouts/DropdownFilterContainer.jsx
+++ src/layouts/DropdownFilterContainer.jsx
@@ -11,7 +11,7 @@ function DropdownFilterContainer({ dropdownName, mainDataLength, content }) {
     <svg className="w-3 cursor-pointer fill-white" aria-hidden="true" focusable="false" role="img" data-icon="minus" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 695 137"><path fillRule="evenodd" d="M68 0h558c38 0 69 31 69 68 0 38-31 69-69 69H68c-37 0-68-31-68-69C0 31 31 0 68 0Z" clipRule="evenodd" /></svg>
    </div>
 
-   <div className={`bg-background-100 rounded-md overflow-y-auto overflow-scroll-dropdown-filter shadow-md z-30 closest-${_.camelCase(dropdownName)}-filter max-md:w-[90vw] ${mainDataLength > 8 && 'max-h-[60vh] pl-2 pr-2'} py-2 px-2`}>
+   <div className={`bg-background-100 rounded-md overflow-y-auto overflow-scroll-dropdown-filter shadow-md closest-${_.camelCase(dropdownName)}-filter max-md:w-[90vw] ${mainDataLength > 8 && 'max-h-[60vh] pl-2 pr-2'} py-2 px-2`}>
     {content}
    </div>
   </div>
